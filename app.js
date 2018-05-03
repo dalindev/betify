c74999f67d5fbc224b2598c2458295180c509908
dalinhuang99 <dhuan023@gmail.com>
2018-05-02 22:16:45 -0400

    reduce write frequency to approximately 10 seconds

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ var dbconfig = require('./config/database');
 var connection = mysql.createConnection(dbconfig.connection);
 
 connection.connect(function(err) {
-  if (err) throw err
-  console.log('Looks Good! You are now connected...')
+	if (err) throw err
+	console.log('Looks Good! You are now connected...')
 })
 
 /* -------------------------------------------
-  Public file
+	Public file
 ------------------------------------------- */
 app.use(express.static(__dirname + '/public'));
 
@@ -48,7 +48,7 @@ app.use(cookieParser()); // read cookies (needed for auth)
 
  // to support URL-encoded bodies
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 app.use(bodyParser.json());
 
@@ -56,9 +56,9 @@ app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
 app.use(session({
-  secret: config.session_secret,
-  resave: true,
-  saveUninitialized: true
+	secret: config.session_secret,
+	resave: true,
+	saveUninitialized: true
  } )); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
@@ -66,7 +66,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 
 /* -------------------------------------------
-  API
+	API
 ------------------------------------------- */
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
@@ -75,30 +75,30 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 //app.listen(port);
 var server = app.listen(port, "0.0.0.0", function () {
 
-  let host = server.address().address
-  let port = server.address().port
+	let host = server.address().address
+	let port = server.address().port
 
-  console.log("Server started: Betify at http://%s:%s", host, port);
+	console.log("Server started: Betify at http://%s:%s", host, port);
 
-  // initialize the /sse route
-  var sse = new SSE(server);
+	// initialize the /sse route
+	var sse = new SSE(server);
 
-  // When a connection is made
-  sse.on('connection', function(stream) {
-    console.log('Opened connection ');
-    clients.push(stream);
+	// When a connection is made
+	sse.on('connection', function(stream) {
+		console.log('Opened connection ');
+		clients.push(stream);
 
-    // Send data back to the client
-    var json = JSON.stringify({ message: 'Gotcha' });
-    stream.send(json);
-    console.log('Sent: ' + json);
+		// Send data back to the client
+		var json = JSON.stringify({ message: 'Gotcha' });
+		stream.send(json);
+		console.log('Sent: ' + json);
 
-    // The connection was closed
-    stream.on('close', function() {
-      clients.splice(clients.indexOf(stream), 1);
-      console.log('Closed connection ');
-    });
-  });
+		// The connection was closed
+		stream.on('close', function() {
+			clients.splice(clients.indexOf(stream), 1);
+			console.log('Closed connection ');
+		});
+	});
 
 });
 
@@ -109,10 +109,10 @@ var server = app.listen(port, "0.0.0.0", function () {
 
 const binance = require('node-binance-api');
 binance.options({
-  APIKEY: '<key>',
-  APISECRET: '<secret>',
-  useServerTime: true, // If you get timestamp errors, synchronize to server time at startup
-  test: false // If you want to use sandbox mode where orders are simulated
+	APIKEY: '<key>',
+	APISECRET: '<secret>',
+	useServerTime: true, // If you get timestamp errors, synchronize to server time at startup
+	test: false // If you want to use sandbox mode where orders are simulated
 });
 
 
@@ -122,76 +122,92 @@ var startBtcPrice = 0;
 var endBtcPrice = 0;
 // For a specific symbol:
 binance.websockets.prevDay('BTCUSDT', (error, response) => {
-  // var t = new Date( response.eventTime );
-  // var formatted = t.format("dd.mm.yyyy hh:MM:ss");
+	// var t = new Date( response.eventTime );
+	// var formatted = t.format("dd.mm.yyyy hh:MM:ss");
 
-  // save to database
-  if(response.symbol && response.close && response.closeTime && response.closeQty){
+	// save to database
+	if(response.symbol && response.close && response.closeTime && response.closeQty){
 
-    let closeTime = moment(response.closeTime).format("HH:mm:ss");
-    let closePrice = response.close;
+		let closeTime = moment(response.closeTime).format("HH:mm:ss");
+		let closePrice = response.close;
 
-    // game loop counter
-    if(gameTimeCounter == 0) {
-      startBtcPrice = response.close;
-      gameTimeCounter++;
-    } else if(gameTimeCounter == 9){
-      gameTimeCounter = 0;
-      endBtcPrice = closePrice;
-    } else {
-      gameTimeCounter++;
-    }
+		// game loop counter
+		if(gameTimeCounter === 0) {
+			startBtcPrice = response.close;
+			gameTimeCounter++;
+		} else if(gameTimeCounter === 9){
+			gameTimeCounter = 0;
+			endBtcPrice = closePrice;
+		} else {
+			gameTimeCounter++;
+		}
 
-    let btcusdtMysql = {
-          exchange_name: 'Binance',
-          symbol: response.symbol,
-          close_price: response.close,
-          close_time: response.closeTime,
-          game_time_counter: gameTimeCounter,
-          close_qty: response.closeQty
-        };
+		// game start or game end
+		if(gameTimeCounter === 0 || gameTimeCounter === 9){
+			let btcusdtMysql = {
+						exchange_name: 'Binance',
+						symbol: response.symbol,
+						close_price: response.close,
+						close_time: response.closeTime,
+						game_time_counter: gameTimeCounter,
+						close_qty: response.closeQty
+					};
 
-    let insertQuery = "INSERT INTO btcusdt_table (exchange_name, symbol, close_price, close_time, game_time_counter, close_qty) values (?,?,?,?,?,?)";
-    connection.query(insertQuery,[
-                        btcusdtMysql.exchange_name,
-                        btcusdtMysql.symbol,
-                        btcusdtMysql.close_price,
-                        btcusdtMysql.close_time,
-                        btcusdtMysql.game_time_counter,
-                        btcusdtMysql.close_qty
-                      ],function(err, rows) {
-                        if(err) {
-                          console.log('error save btcusdt_table --------->'+err);
-                        } else {
-                          // console.log(response);
-                        connection.query('SELECT * FROM addresses ', function (error, results, fields) {
-                          if (error) throw error;
-                            // res.end(
-                              JSON.stringify('error ' + results)
-                            // );
-                          });
-
-                          broadcast(closeTime, closePrice, gameTimeCounter, startBtcPrice, endBtcPrice);
-                        }
-                      });
-    }
-  });
+			let insertQuery = `INSERT INTO
+									btcusdt_table (
+										exchange_name,
+										symbol,
+										close_price,
+										close_time,
+										game_time_counter,
+										close_qty
+									) values (?,?,?,?,?,?)`;
+			connection.query(
+				insertQuery,[
+					btcusdtMysql.exchange_name,
+					btcusdtMysql.symbol,
+					btcusdtMysql.close_price,
+					btcusdtMysql.close_time,
+					btcusdtMysql.game_time_counter,
+					btcusdtMysql.close_qty
+				],function(err, rows) {
+					if(err) {
+						console.log('error save btcusdt_table --------->'+err);
+					} else {
+						// console.log(response);
+					connection.query('SELECT id FROM btcusdt_table ORDER BY id DESC LIMIT 1', function (error, results, fields) {
+						if (error) throw error;
+							// res.end(
+								JSON.stringify('error ' + results)
+							// );
+						});
+						console.log(JSON.stringify('res--> ' + results));
+						broadcast(closeTime, closePrice, gameTimeCounter, startBtcPrice, endBtcPrice);
+					}
+			})
+		}
+		// during the game
+		else {
+			broadcast(closeTime, closePrice, gameTimeCounter, startBtcPrice, endBtcPrice);
+		}
+	}
+});
 
 
 // Every three seconds broadcast "{ message: 'Hello hello!' }" to all connected clients
 var broadcast = function(time,close,gameTimeCounter,startBtcPrice,endBtcPrice) {
-  var json = JSON.stringify({
-                  eventTime: time,
-                  close: close,
-                  gameTimeCounter: gameTimeCounter,
-                  startBtcPrice: startBtcPrice,
-                  endBtcPrice: endBtcPrice
-                });
+	var json = JSON.stringify({
+					eventTime: time,
+					close: close,
+					gameTimeCounter: gameTimeCounter,
+					startBtcPrice: startBtcPrice,
+					endBtcPrice: endBtcPrice
+				});
 
-  clients.forEach(function(stream) {
-    stream.send(json);
-    // console.log('Sent: ' + json);
-  });
+	clients.forEach(function(stream) {
+		stream.send(json);
+		// console.log('Sent: ' + json);
+	});
 };
 
 
--- config/passport.js
+++ config/passport.js
@@ -13,119 +13,119 @@ connection.query('USE ' + dbconfig.database);
 // expose this function to our app using module.exports
 module.exports = function(passport) {
 
-  // =========================================================================
-  // passport session setup ==================================================
-  // =========================================================================
-  // required for persistent login sessions
-  // passport needs ability to serialize and unserialize users out of session
+	// =========================================================================
+	// passport session setup ==================================================
+	// =========================================================================
+	// required for persistent login sessions
+	// passport needs ability to serialize and unserialize users out of session
 
-  // used to serialize the user for the session
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
+	// used to serialize the user for the session
+	passport.serializeUser(function(user, done) {
+	done(null, user.id);
+	});
 
-  // used to deserialize the user
-  passport.deserializeUser(function(id, done) {
-    connection.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
-      done(err, rows[0]);
-    });
-  });
+	// used to deserialize the user
+	passport.deserializeUser(function(id, done) {
+	connection.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
+		done(err, rows[0]);
+	});
+	});
 
-  // =========================================================================
-  // LOCAL SIGNUP ============================================================
-  // =========================================================================
-  // we are using named strategies since we have one for login and one for signup
-  // by default, if there was no name, it would just be called 'local'
+	// =========================================================================
+	// LOCAL SIGNUP ============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
 
-  passport.use(
-    'local-signup',
-    new LocalStrategy({
-      // by default, local strategy uses username and password, we will override with email
-      usernameField : 'username',
-      passwordField : 'password',
-      passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, username, password, done) {
-      console.log('req-->' + JSON.stringify(req.body));
-      // find a user whose email is the same as the forms email
-      // we are checking to see if the user trying to login already exists
-      connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows) {
-        if (err)
-          return done(err);
-        if (rows.length) {
-          return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-        } else {
-          // if there is no user with that username
-          // create the user
-          var newUserMysql = {
-            username: username,
-            password: bcrypt.hashSync(password, null, null),  // use the generateHash function in our user model
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            email: req.body.email,
-            phone: req.body.phone,
-            city: req.body.city,
-            prov: req.body.prov,
-            country: req.body.country,
-            postal_code: req.body.postal_code
-          };
+	passport.use(
+	'local-signup',
+	new LocalStrategy({
+		// by default, local strategy uses username and password, we will override with email
+		usernameField : 'username',
+		passwordField : 'password',
+		passReqToCallback : true // allows us to pass back the entire request to the callback
+	},
+	function(req, username, password, done) {
+		console.log('req-->' + JSON.stringify(req.body));
+		// find a user whose email is the same as the forms email
+		// we are checking to see if the user trying to login already exists
+		connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows) {
+		if (err)
+			return done(err);
+		if (rows.length) {
+			return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+		} else {
+			// if there is no user with that username
+			// create the user
+			var newUserMysql = {
+			username: username,
+			password: bcrypt.hashSync(password, null, null),  // use the generateHash function in our user model
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			email: req.body.email,
+			phone: req.body.phone,
+			city: req.body.city,
+			prov: req.body.prov,
+			country: req.body.country,
+			postal_code: req.body.postal_code
+			};
 
-          var insertQuery = "INSERT INTO users ( username, password, firstname, lastname, email, phone, city, prov, country, postal_code ) values (?,?,?,?,?,?,?,?,?,?)";
+			var insertQuery = "INSERT INTO users ( username, password, firstname, lastname, email, phone, city, prov, country, postal_code ) values (?,?,?,?,?,?,?,?,?,?)";
 
-          connection.query(insertQuery,[  newUserMysql.username,
-                          newUserMysql.password,
-                          newUserMysql.firstname,
-                          newUserMysql.lastname,
-                          newUserMysql.email,
-                          newUserMysql.phone,
-                          newUserMysql.city,
-                          newUserMysql.prov,
-                          newUserMysql.country,
-                          newUserMysql.postal_code
-                        ],function(err, rows) {
-            newUserMysql.id = rows.insertId;
+			connection.query(insertQuery,[  newUserMysql.username,
+							newUserMysql.password,
+							newUserMysql.firstname,
+							newUserMysql.lastname,
+							newUserMysql.email,
+							newUserMysql.phone,
+							newUserMysql.city,
+							newUserMysql.prov,
+							newUserMysql.country,
+							newUserMysql.postal_code
+						],function(err, rows) {
+			newUserMysql.id = rows.insertId;
 
-            return done(null, newUserMysql);
-          });
-        }
-      });
-    })
-  );
+			return done(null, newUserMysql);
+			});
+		}
+		});
+	})
+	);
 
-  // =========================================================================
-  // LOCAL PROFILE EDIT ======================================================
-  // =========================================================================
+	// =========================================================================
+	// LOCAL PROFILE EDIT ======================================================
+	// =========================================================================
 
 
-  // =========================================================================
-  // LOCAL LOGIN =============================================================
-  // =========================================================================
-  // we are using named strategies since we have one for login and one for signup
-  // by default, if there was no name, it would just be called 'local'
+	// =========================================================================
+	// LOCAL LOGIN =============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
 
-  passport.use(
-    'local-login',
-    new LocalStrategy({
-      // by default, local strategy uses username and password, we will override with email
-      usernameField : 'username',
-      passwordField : 'password',
-      passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, username, password, done) { // callback with email and password from our form
-      connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows){
-        if (err)
-          return done(err);
-        if (!rows.length) {
-          return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-        }
+	passport.use(
+	'local-login',
+	new LocalStrategy({
+		// by default, local strategy uses username and password, we will override with email
+		usernameField : 'username',
+		passwordField : 'password',
+		passReqToCallback : true // allows us to pass back the entire request to the callback
+	},
+	function(req, username, password, done) { // callback with email and password from our form
+		connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows){
+		if (err)
+			return done(err);
+		if (!rows.length) {
+			return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+		}
 
-        // if the user is found but the password is wrong
-        if (!bcrypt.compareSync(password, rows[0].password))
-          return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+		// if the user is found but the password is wrong
+		if (!bcrypt.compareSync(password, rows[0].password))
+			return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-        // all is well, return successful user
-        return done(null, rows[0]);
-      });
-    })
-  );
+		// all is well, return successful user
+		return done(null, rows[0]);
+		});
+	})
+	);
 };
